ec697fb7ab1e236160809d63b8efc3342ea125e1
Douglas Dantas <douglas.mdantas@hotmail.com>
2019-02-18 19:36:24 -0300

    Iniciado o servidor e adicionado o gitignore

---
+++ server.js
@@ -0,0 +1,16 @@
+const express  = require('express')
+const app = express()
+
+app.listen(3000, function(){
+    console.log('Server is runnig on port 3000')
+})
+
+//PRIMEIRO ARGUMENTO DO GET PEGA O CAMINHO NO QUAL O NAVEGADOR  ESTÁ ACESSANDO
+/*
+**SEGUNDO ARGUMENTO DO GET É UM CALLBACK QUE INFORMA O 
+**SERVIDOR O QUE FAZER QUANDO FOR RESPONDIDO PELO NAVEGADOR
+**ESSE CALLBACK CONTÉM 2 ARGUMENTOS: UMA REQUISIÇÃO E UMA RESPOSTA(req, res)
+*/
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
