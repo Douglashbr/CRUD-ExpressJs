1737a12aefc9986fde4aa75a80fed1d420a98337
Douglas Dantas <douglas.mdantas@hotmail.com>
2019-02-18 19:44:12 -0300

    Adicionado formulário index.ejs

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express  = require('express')
 const app = express()
 
+
+//CHAMAR TEMPLATE ENGINE EJS(EMBEDDED JAVASCRIPT)
+
+app.set('view engine', 'ejs')
+
 app.listen(3000, function(){
     console.log('Server is runnig on port 3000')
 })
@@ -12,5 +17,5 @@ app.listen(3000, function(){
 **ESSE CALLBACK CONTÉM 2 ARGUMENTOS: UMA REQUISIÇÃO E UMA RESPOSTA(req, res)
 */
 app.get('/', (req, res) => {
-    res.send('Hello World')
+    res.render('index.ejs')
 })
